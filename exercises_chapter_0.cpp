90fa84c5c5efae6a8f62dc5f1f934da423ff8060
Alok Subbarao <aloksubbarao@gmail.com>
2018-01-15 00:02:20 -0800

    finished chapter 0

---
+++ exercises_chapter_0.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int main()
+
+{
+//0-1 std::cout<<3+4<<std::endl;s
+//0-2 std::cout<< "This (\") is a quote, \t and this (\\) is a backslash"<<std::endl;
+
+//0-4std::cout << "# include <iostream> \n int main() \n{ \nstd::cout<<\"Hello, World!\""<<std::endl;
+//0-4std::cout << "return 0;\n}"<<std::endl;
+
+return 0;
+
+
+
+}
